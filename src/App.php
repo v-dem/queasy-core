41a677264540c364dd70d0e3b454d3b74cb93a27
v-dem <vitaly_demyanenko@yahoo.com>
2017-09-10 18:20:01 +0300

    added gitignore

--- src/App.php
+++ src/App.php
@@ -2,8 +2,6 @@
 
 namespace queasy;
 
-use queasy\log\Logger;
-
 class App
 {
 
@@ -31,32 +29,28 @@ class App
      */
     public function handle(HttpRequest $request)
     {
-        try {
-            $routeClass = Loader::load('route');
+        $routeClass = Loader::load('route');
 
-            $route = new $routeClass($this->route, $request->method());
-            $controllerClassMethod = $route->resolve();
-            if (false === $controllerClassMethod) {
-                throw new Exception('Can\t resolve route.'); // TODO: Change to 404 response
-            }
+        $route = new $routeClass($this->route, $request->method());
+        $controllerClassMethod = $route->resolve();
+        if (false === $controllerClassMethod) {
+            throw new Exception('Can\t resolve route.'); // TODO: Change to 404 response
+        }
 
-            $controllerClass = array_shift($controllerClassMethod);
-            $controllerMethod = array_shift($controllerClassMethod);
+        $controllerClass = array_shift($controllerClassMethod);
+        $controllerMethod = array_shift($controllerClassMethod);
 
-            $controller = new $controllerClass($request);
-            if (method_exists($controller, $controllerMethod)) {
-                $output = call_user_func_array(array($controller, $controllerMethod), $route->get());
-            } else {
-                throw new ApplicationException(sprintf('Method "%s" doesn\'t exists in class "%s".', $method, $controllerClass)); // TODO: Change to 404 response
-            }
+        $controller = new $controllerClass($request);
+        if (method_exists($controller, $controllerMethod)) {
+            $output = call_user_func_array(array($controller, $controllerMethod), $route->get());
+        } else {
+            throw new ApplicationException(sprintf('Method "%s" doesn\'t exists in class "%s".', $method, $controllerClass)); // TODO: Change to 404 response
+        }
 
-            if ($request->isAjax()) {
-                return json_encode($output);
-            } else {
-                return $output;
-            }
-        } catch (Exception $e) { // TODO: Improve exceptions handling
-            Logger::error($e->getMessage());
+        if ($request->isAjax()) {
+            return json_encode($output);
+        } else {
+            return $output;
         }
     }
 
